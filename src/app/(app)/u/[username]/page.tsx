2ab215458323b3ee953f2e8ec7a74e04d0c4c7be
Samir Khanal <gdssamir@gmail.com>
2024-12-31 22:04:01 +0545

    Remove unused components and API for message suggestions

--- src/app/(app)/u/[username]/page.tsx
+++ src/app/(app)/u/[username]/page.tsx
@@ -7,8 +7,8 @@ import { useForm } from "react-hook-form";
 import { Loader2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { CardHeader, CardContent, Card } from "@/components/ui/card";
-import { useCompletion } from "ai/react";
+import { CardHeader, Card } from "@/components/ui/card";
+
 import {
   Form,
   FormControl,
@@ -25,38 +25,15 @@ import Link from "next/link";
 import { useParams } from "next/navigation";
 import { MessagesSchema as messageSchema } from "@/schemas/messageSchema";
 
-const specialChar = "||";
-
-const parseStringMessages = (messageString: string): string[] => {
-  return messageString.split(specialChar);
-};
-
-const initialMessageString =
-  "What's your favorite movie?||Do you have any pets?||What's your dream job?";
-
 export default function SendMessage() {
   const params = useParams<{ username: string }>();
   const username = params.username;
-
-  // const {
-  //   complete,
-  //   completion,
-  //   isLoading: isSuggestLoading,
-  //   error,
-  // } = useCompletion({
-  //   api: "/api/suggest-messages",
-  //   initialCompletion: initialMessageString,
-  // });
 
   const form = useForm<z.infer<typeof messageSchema>>({
     resolver: zodResolver(messageSchema),
   });
 
   const messageContent = form.watch("content");
-
-  const handleMessageClick = (message: string) => {
-    form.setValue("content", message);
-  };
 
   const [isLoading, setIsLoading] = useState(false);
 
@@ -85,15 +62,6 @@ export default function SendMessage() {
       setIsLoading(false);
     }
   };
-
-  // const fetchSuggestedMessages = async () => {
-  //   try {
-  //     complete('');
-  //   } catch (error) {
-  //     console.error('Error fetching messages:', error);
-  //     // Handle error appropriately
-  //   }
-  // };
 
   return (
     <div className="container mx-auto my-8 p-6 bg-white rounded max-w-4xl">
@@ -133,47 +101,6 @@ export default function SendMessage() {
           </div>
         </form>
       </Form>
-
-      <div className="space-y-4 my-8">
-        <div className="space-y-2">
-          <Button
-            // onClick={fetchSuggestedMessages}
-            className="my-4"
-            // disabled={isSuggestLoading}
-          >
-            Suggest Messages
-          </Button>
-          <p>Click on any message below to select it.</p>
-        </div>
-        <Card>
-          <CardHeader>
-            <h3 className="text-xl font-semibold">Messages</h3>
-          </CardHeader>
-          {/* <CardContent className="flex flex-col space-y-4">
-            {error ? (
-              <p className="text-red-500">{error.message}</p>
-            ) : (
-              parseStringMessages(completion).map((message, index) => (
-                <Button
-                  key={index}
-                  variant="outline"
-                  className="mb-2"
-                  onClick={() => handleMessageClick(message)}
-                >
-                  {message}
-                </Button>
-              ))
-            )}
-          </CardContent> */}
-        </Card>
-      </div>
-      <Separator className="my-6" />
-      <div className="text-center">
-        <div className="mb-4">Get Your Message Board</div>
-        <Link href={"/sign-up"}>
-          <Button>Create Your Account</Button>
-        </Link>
-      </div>
     </div>
   );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -12,7 +12,8 @@ export async function middleware(request: NextRequest) {
     token &&
     (url.pathname.startsWith("/sign-in") ||
       url.pathname.startsWith("/sign-up") ||
-      url.pathname.startsWith("/verify"))
+      url.pathname.startsWith("/verify") ||
+      url.pathname === "/")
   ) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
